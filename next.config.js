01046247964b8cf8c297bc07ec6a073f544ffc4e
nguyennhuyy <huy.reactjs@gmail.com>
2024-03-20 22:09:50 +0700

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require("next-pwa")({
+	dest: "public",
+	skipWaiting: true,
+	register: true,
+	cacheOnFrontEndNav: true,
+	reloadOnOnline: true,
+	disable: process.env.NODE_ENV === "development"
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
@@ -15,4 +24,4 @@ const nextConfig = {
 	}
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
