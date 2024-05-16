246946bb37760811362ab1c94d0b67366c1e5f62
Nguyen Huy <nguyenhuy@quorumex.io>
2024-05-16 11:50:35 +0700

    update force update page

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,51 +1,52 @@
-import React, { useContext } from "react";
-import { useGoogleLogin } from "@react-oauth/google";
-import Image from "next/image";
-import { toast } from "sonner";
+import React, { useContext } from "react"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { useCookie } from "@/hooks"
+import { KeyCookie } from "@/lib"
+import { useGoogleLogin } from "@react-oauth/google"
+import { toast } from "sonner"
 
-import { Button } from "../ui";
-import { useLoginGoogleMutation } from ".";
-import { AuthContext } from "./context";
-import { useRouter } from "next/navigation";
-import { useCookie } from "@/hooks";
-import { KeyCookie } from "@/lib";
+import { useLoginGoogleMutation } from "."
+import { Button } from "../ui"
+import { AuthContext } from "./context"
 
 const Social = () => {
-	const router = useRouter();
-	const { handleOpenDialog } = useContext(AuthContext);
+  const router = useRouter()
+  const { handleOpenDialog } = useContext(AuthContext)
 
-	const { setItemCookie } = useCookie();
+  const { setItemCookie } = useCookie()
 
-	const { mutateAsync: loginGoogle } = useLoginGoogleMutation();
+  const { mutateAsync: loginGoogle } = useLoginGoogleMutation()
 
-	const login = useGoogleLogin({
-		onSuccess: tokenResponse => {
-			loginGoogle({ accessToken: tokenResponse.access_token }).then(
-				response => {
-					setItemCookie(KeyCookie.Token, response?.accessToken);
-					toast.success("Đăng nhập thành công");
-					handleOpenDialog?.();
-					router.push("/documents");
-				}
-			);
-		}
-	});
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      loginGoogle({ accessToken: tokenResponse.access_token }).then(
+        (response) => {
+          setItemCookie(KeyCookie.Token, response?.accessToken)
+          toast.success("Đăng nhập thành công")
+          handleOpenDialog?.()
+          location.href = "/documents"
+        }
+      )
+    },
+  })
 
-	return (
-		<div className='flex flex-col gap-2 !mt-8'>
-			<Button
-				onClick={() => login()}
-				variant='outline'
-				className='py-4 gap-2 justify-start'>
-				<Image width={25} height={25} src='/google.svg' alt='google' />
-				<span>Đăng nhập với Google</span>
-			</Button>
-			<Button variant='outline' className='py-4 gap-2 justify-start'>
-				<Image width={25} height={25} src='/facebook.svg' alt='google' />
-				<span>Đăng nhập với Facebook </span>
-			</Button>
-		</div>
-	);
-};
+  return (
+    <div className="flex flex-col gap-2 !mt-8">
+      <Button
+        onClick={() => login()}
+        variant="outline"
+        className="py-4 gap-2 justify-start"
+      >
+        <Image width={25} height={25} src="/google.svg" alt="google" />
+        <span>Đăng nhập với Google</span>
+      </Button>
+      <Button variant="outline" className="py-4 gap-2 justify-start">
+        <Image width={25} height={25} src="/facebook.svg" alt="google" />
+        <span>Đăng nhập với Facebook </span>
+      </Button>
+    </div>
+  )
+}
 
-export default Social;
+export default Social
